2f833bfe71dceb3e2abce4a5b3e34c2137566643
Sébastien Combéfis <seb478@gmail.com>
2016-10-21 11:49:16 +0200

    Change application name in page title

--- modules/core/client/directives/page-title.client.directives.js
+++ modules/core/client/directives/page-title.client.directives.js
@@ -25,7 +25,7 @@
       }
 
       function getTitle(currentState) {
-        var applicationCoreTitle = 'Pythia';
+        var applicationCoreTitle = 'OpenSM';
         var workingState = currentState;
         if (currentState.data) {
           workingState = (typeof workingState.locals !== 'undefined') ? workingState.locals.globals : workingState;
