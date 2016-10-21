885cbf54f3904a28d67707bbae0a24001e5148b2
Sébastien Combéfis <seb478@gmail.com>
2016-10-21 15:28:00 +0200

    Remove unrelated module example

--- modules/problems/client/problems.client.module.js
+++
@@ -1,7 +0,0 @@
-(function (app) {
-  'use strict';
-
-  app.registerModule('problems', ['core']);// The core module is required for special route handling; see /core/client/config/core.client.routes
-  app.registerModule('problems.services');
-  app.registerModule('problems.routes', ['ui.router', 'core.routes', 'problems.services']);
-}(ApplicationConfiguration));
