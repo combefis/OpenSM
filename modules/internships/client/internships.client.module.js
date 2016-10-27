6e6a5652e3d778709e08a11d799da7122158878d
Damien Vanhove <VanhoveDamien@hotmail.com>
2016-10-27 20:29:39 +0200

    Modules(internships): Modify the client menu capitalisation
    - Modified capitalisation in client.routes.
    - Modified the client HTML view to get the student name and not roomid (copy paste error)

--- modules/internships/client/internships.client.module.js
+++ modules/internships/client/internships.client.module.js
@@ -3,5 +3,5 @@
 
   app.registerModule('internships', ['core']);// The core module is required for special route handling; see /core/client/config/core.client.routes
   app.registerModule('internships.services');
-  app.registerModule('internships.routes', ['ui.router', 'core.routes', 'rooms.services']);
+  app.registerModule('internships.routes', ['ui.router', 'core.routes', 'internships.services']);
 }(ApplicationConfiguration));
