b6cde85d2cf671eafbdee60c45b3e424a120c8f7
Damien Vanhove <VanhoveDamien@hotmail.com>
2016-10-27 19:14:39 +0200

    Module(internships) Add internships.client.routes.

---
+++ modules/internships/client/config/internships.client.routes.js
@@ -0,0 +1,27 @@
+(function () {
+  'use strict';
+
+  angular
+    .module('internships.routes')
+    .config(routeConfig);
+
+  routeConfig.$inject = ['$stateProvider'];
+
+  function routeConfig($stateProvider) {
+    $stateProvider
+      .state('admin.manage.internships', {
+        abstract: true,
+        url: '/internships',
+        template: '<ui-view/>'
+      })
+      .state('admin.manage.internships.list', {
+        url: '',
+        templateUrl: 'modules/rooms/client/views/list-rooms.client.view.html',
+        controller: 'internshipsListController',
+        controllerAs: 'vm',
+        data: {
+          pageTitle: 'internships list'
+        }
+      });
+  }
+}());
