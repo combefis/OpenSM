00a72b21a22553ba0adb396a2b495f20940a8532
Sébastien Combéfis <seb478@gmail.com>
2016-11-26 13:52:10 +0100

    Modules(exams): Add server side

---
+++ modules/exams/server/controllers/exams.server.controller.js
@@ -0,0 +1,98 @@
+'use strict';
+
+/**
+ * Module dependencies
+ */
+var path = require('path'),
+  moment = require('moment'),
+  mongoose = require('mongoose'),
+  Exam = mongoose.model('Exam'),
+  errorHandler = require(path.resolve('./modules/core/server/controllers/errors.server.controller'));
+
+/**
+ * Create an exam
+ */
+exports.create = function (req, res) {
+  var exam = new Exam(req.body);
+  exam.user = req.user;
+  exam.academicyear = 2016;
+
+  // Save the exam
+  exam.save(function (err) {
+    if (err) {
+      return res.status(400).send({
+        message: errorHandler.getErrorMessage(err)
+      });
+    }
+    res.json(exam);
+  });
+};
+
+/**
+ * Show the current exam
+ */
+exports.read = function (req, res) {
+  // convert mongoose document to JSON
+  var exam = req.exam ? req.exam.toJSON() : {};
+  res.json(exam);
+};
+
+/**
+ * Update an exam
+ */
+exports.update = function (req, res) {
+  var exam = req.exam;
+
+  exam.title = req.body.title;
+  exam.course = req.body.course;
+  exam.examsession = req.body.examsession;
+  exam.date = req.body.date;
+  exam.duration = req.body.duration;
+
+  exam.save(function (err) {
+    if (err) {
+      return res.status(422).send({
+        message: errorHandler.getErrorMessage(err)
+      });
+    }
+    res.json(exam);
+  });
+};
+
+/**
+ * List of exams
+ */
+exports.list = function (req, res) {
+  Exam.find({ 'academicyear': req.session.academicyear }).sort({ date: 1 }).exec(function (err, exams) {
+    if (err) {
+      return res.status(400).send({
+        message: errorHandler.getErrorMessage(err)
+      });
+    }
+    res.json(exams);
+  });
+};
+
+/**
+ * Exam middleware
+ */
+exports.examByID = function (req, res, next, id) {
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(400).send({
+      message: 'Exam is invalid'
+    });
+  }
+
+  Exam.findById(id, 'title course examsession date duration').exec(function (err, exam) {
+    if (err) {
+      return next(err);
+    }
+    if (!exam) {
+      return res.status(404).send({
+        message: 'No exam with that identifier has been found.'
+      });
+    }
+    req.exam = exam;
+    next();
+  });
+};
---
+++ modules/exams/server/routes/exams.server.routes.js
@@ -0,0 +1,22 @@
+'use strict';
+
+/**
+ * Module dependencies
+ */
+var examsPolicy = require('../policies/exams.server.policy'),
+  exams = require('../controllers/exams.server.controller');
+
+module.exports = function (app) {
+  // Exams collection routes
+  app.route('/api/exams').all(examsPolicy.isAllowed)
+    .get(exams.list)
+    .post(exams.create);
+
+  // Single exam routes
+  app.route('/api/exams/:examId').all(examsPolicy.isAllowed)
+    .get(exams.read)
+    .put(exams.update);
+
+  // Finish by binding the exam middleware
+  app.param('examId', exams.examByID);
+};
