519fef2904aa1729c7adf6a23cfb73df8cbbf6f1
Sébastien Combéfis <seb478@gmail.com>
2016-11-26 11:53:07 +0100

    Modules(exams): Add model description

---
+++ modules/exams/server/models/exam.server.model.js
@@ -0,0 +1,47 @@
+'use strict';
+
+/**
+ * Module dependencies
+ */
+var mongoose = require('mongoose'),
+  Schema = mongoose.Schema;
+
+/**
+ * Exam Schema
+ */
+var ExamSchema = new Schema({
+  title: {
+    type: String,
+    trim: true
+  },
+  course: {
+    type: Schema.ObjectId,
+    ref: 'Course',
+    required: 'Please fill in the course of the exam.'
+  },
+  examsession: {
+    type: Schema.ObjectId,
+    ref: 'ExamSession'
+  },
+  date: {
+    type: Date,
+    required: 'Please fill in the date of the exam.'
+  },
+  duration: {
+    type: Number,
+    required: 'Please fill in the duration of the exam.'
+  },
+  academicyear: {
+    type: Number
+  },
+  created: {
+    type: Date,
+    default: Date.now
+  },
+  user: {
+    type: Schema.ObjectId,
+    ref: 'User'
+  }
+});
+
+mongoose.model('Exam', ExamSchema);
