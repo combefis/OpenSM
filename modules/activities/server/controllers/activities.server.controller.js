7c2f0a9a3dbce6d211529a7ed72c0b3b1a8de631
Sébastien Combéfis <seb478@gmail.com>
2016-12-10 11:42:20 +0100

    Modules(activities): Order by code in activity list

--- modules/activities/server/controllers/activities.server.controller.js
+++ modules/activities/server/controllers/activities.server.controller.js
@@ -60,8 +60,9 @@ exports.update = function (req, res) {
  * List of activities
  */
 exports.list = function (req, res) {
-  Activity.find({ 'academicyear': req.session.academicyear })
+  Activity.find({ academicyear: req.session.academicyear })
   .populate('teachers', 'displayName')
+  .sort({ code: 1 })
   .exec(function (err, activities) {
     if (err) {
       return res.status(400).send({
