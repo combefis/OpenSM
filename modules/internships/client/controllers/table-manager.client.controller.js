abc916bdeb332a08502f974d0f17adc44c8a8486
Damien Vanhove <vanhovedamien@hotmail.com>
2017-05-18 22:54:07 +0200

    Modules(internships): Change table view for Manager

--- modules/internships/client/controllers/table-manager.client.controller.js
+++ modules/internships/client/controllers/table-manager.client.controller.js
@@ -5,12 +5,137 @@
     .module('internships')
     .controller('InternshipsManagerTableController', InternshipsManagerTableController);
 
-  InternshipsManagerTableController.$inject = ['$scope', '$state', '$window', 'Authentication', '$http', '$filter', 'InternshipsService'];
+  InternshipsManagerTableController.$inject = ['$scope', '$state', '$window', 'Authentication', '$http', '$filter', 'InternshipsService', 'DeadlinesService', '$rootScope', 'deadlinesResolve'];
 
-  function InternshipsManagerTableController($scope, $state, $window, Authentication, $http, filter, InternshipsService) {
+  function InternshipsManagerTableController($scope, $state, $window, Authentication, $http, filter, InternshipsService, DeadlinesService, $rootScope, deadlinesResolve) {
     var vm = this; // on instancie tout ce qu'on vient de lui passer
+    vm.saveDates = saveDates;
+    vm.updateSelection = updateSelection;
 
-    vm.internships = InternshipsService.query();  // il appelle(crer) le service et il fait query (ce qui appelle un GET dans le service)
+    vm.internships = InternshipsService.query(function(internships) {
+      internships.forEach(function(internship) {
+        if (!internship.deadlines) {internship.deadlines = {};}
+        if (internship.deadlines.startInternship) {
+          internship.deadlines.startInternship = new Date(internship.deadlines.startInternship);
+        }
+        if (internship.deadlines.endInternship) {
+          internship.deadlines.endInternship = new Date(internship.deadlines.endInternship);
+        }
+        internship.startPopup = { opened: false };
+        internship.endPopup = { opened: false };
+      });
+    });
+
+    function saveDates() {
+
+
+      $http.put('/api/internships/startEndDates', { 'internships': vm.internships }).success(successCallback);
+
+      function successCallback(res) {
+        alert('Success! internships updated.');
+      // location.reload();
+      // $state.go('coordinator.manage.internships.list');
+      }
+    }
+
+    vm.deadlinesCollection = DeadlinesService.query();
+    vm.deadlines = deadlinesResolve;
+    vm.open = open;
+
+    vm.validateConventions = validateConventions;
+    vm.giveConventions = giveConventions;
+
+    vm.selectAllValidate = selectAllValidate;
+    vm.selectAllGive = selectAllGive;
+
+    vm.createDeadlines = createDeadlines;
+
+    vm.filterSelect = 'overview';
+    vm.filterOptions = ['overview', 'enterprise', 'proposition', 'supervisor', 'convention', 'deadlines'];
+
+    vm.format = 'yyyy/MM/dd';
+    vm.inlineOptions = {
+      minDate: new Date(),
+      showWeeks: true
+    };
+    vm.dateOptions = {
+      dateDisabled: 'disabled',
+      formatYear: 'yy',
+      maxDate: new Date(2020, 5, 22),
+      minDate: new Date(),
+      startingDay: 1
+    };
+
+    function open(internship, popup, target) {
+      console.log(internship);
+      console.log('popup:' + popup);
+      console.log(target);
+      if (!internship.deadlines) { internship.deadlines = {};}
+      internship.deadlines[target] = new Date();
+      internship[popup].opened = true;
+    }
+
+    function createDeadlines() {
+
+      vm.deadlines.academicYear = prompt("Please enter academic year.", "2016-2017");
+      vm.deadlines.createOrUpdate()          // appel à la fonction dans le service
+        .then(successCallback)
+        .catch(errorCallback);
+
+      function successCallback(res) {
+        console.log('all is good');
+        vm.deadlinesCollection = DeadlinesService.query();
+      }
+
+      function errorCallback(res) {
+        console.log(res);
+        vm.error = res.message.message;
+      }
+    }
+
+    function updateSelection(internship) {
+      $rootScope.internships = vm.internships;
+      console.log($rootScope.internships);
+    }
+
+    function validateConventions(internship) {
+      var internshipsConventionValidateList = [];
+      internship.convention.validation = true;
+      internshipsConventionValidateList.push(internship);
+
+      $http.put('/api/internships/convention', { 'internships': internshipsConventionValidateList }).success(successCallback);
+
+      function successCallback(res) {
+        alert('Success! internships updated.');
+      }
+    }
+
+    function giveConventions(internship) {
+      var internshipsConventionGivenList = [];
+
+      internship.convention.given = true;
+      internshipsConventionGivenList.push(internship);
+
+      $http.put('/api/internships/convention', { 'internships': internshipsConventionGivenList }).success(successCallback);
+
+      function successCallback(res) {
+        alert('Success! internship updated.');
+      }
+    }
+
+    function selectAllValidate() {
+      console.log('selectAll');
+      vm.internships.forEach(function(internship) {
+        internship.checkConventionValid = vm.checkConventionValidateAll;
+      });
+    }
+
+    function selectAllGive() {
+      console.log('selectAll');
+      vm.internships.forEach(function(internship) {
+        internship.checkConventionGiven = vm.checkConventionGiveAll;
+      });
+    }
   }
 
 }());
