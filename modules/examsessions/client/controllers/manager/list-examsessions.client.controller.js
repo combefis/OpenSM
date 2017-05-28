6f7ef69c9dd21a161b6cd6f0d7312cd8b5ed3a9d
Sébastien Combéfis <seb478@gmail.com>
2017-05-28 16:07:20 +0200

    Modules(examsessions): Improve list view

--- modules/examsessions/client/controllers/manager/list-examsessions.client.controller.js
+++ modules/examsessions/client/controllers/manager/list-examsessions.client.controller.js
@@ -10,6 +10,26 @@
   function ExamSessionsListManagerController(ExamSessionsService) {
     var vm = this;
 
-    vm.examsessions = ExamSessionsService.query();
+    vm.examsessions = {
+      loaded: false,
+      past: [],
+      current: [],
+      future: []
+    };
+
+    var examsessions = ExamSessionsService.query(function() {
+      var now = moment();
+      examsessions.forEach(function(examsession) {
+        if (moment(examsession.start).isAfter(now)) {
+          vm.examsessions.future.push(examsession);
+        } else if (now.isAfter(moment(examsession.end))) {
+          vm.examsessions.past.push(examsession);
+        } else {
+          vm.examsessions.current.push(examsession);
+        }
+      });
+
+      vm.examsessions.loaded = true;
+    });
   }
 }());
